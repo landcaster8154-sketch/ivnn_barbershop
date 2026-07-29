173e3f813d3378e2047f303a29702f7225f284c3
landcaster8154-sketch <landcaster8154@gmail.com>
2026-07-29 21:08:19 +0200

    Update messagingSenderId in firebase config

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -5,6 +5,6 @@ window.firebaseConfig = {
   authDomain: "ivnnbarbershop.firebaseapp.com",
   projectId: "ivnnbarbershop",
   storageBucket: "ivnnbarbershop.appspot.com",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID_AQUÍ",
+  messagingSenderId:"475118416238",
   appId: "1:475118416238:web:91ef1d0dab1b41bc0dd17f"
 };
