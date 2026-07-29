100613e63c1c400f2d63fa70ee699b2542cebb97
landcaster8154-sketch <landcaster8154@gmail.com>
2026-07-29 20:46:09 +0200

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,10 @@
 window.FIREBASE_CONFIGURED = true;
-// Your web app's Firebase configuration
-const firebaseConfig = {
+
+window.firebaseConfig = {
   apiKey: "AIzaSyBIgGDJtyFCTmoFGpJHe17TsNPNbgb6aqg",
   authDomain: "ivnnbarbershop.firebaseapp.com",
   projectId: "ivnnbarbershop",
-  storageBucket: "ivnnbarbershop.firebasestorage.app",
-  messagingSenderId: "475118416238",
+  storageBucket: "ivnnbarbershop.appspot.com",
+  messagingSenderId: "TU_MESSAGING_SENDER_ID_AQUÍ",
   appId: "1:475118416238:web:91ef1d0dab1b41bc0dd17f"
 };
